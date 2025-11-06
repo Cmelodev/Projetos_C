0751774cb30a7cb6a264a1a8c3f9ff5369753f6c
Carlos Eduardo de Melo <154885531+Cmelodev@users.noreply.github.com>
2025-11-06 10:57:19 -0300

    att

--- lista_por_arranjo/teste.c
+++ lista_por_arranjo/teste.c
@@ -5,7 +5,7 @@
 #define InicioArranjo 1 
 #define Maxtam 5
 
-typedef int apontador ;
+typedef int apontador;
 
 typedef struct 
 {
@@ -20,7 +20,7 @@ apontador Primeiro;
 apontador Ultimo;
 }tipolista;
 
-tipolista lista ;
+tipolista Lista;
 tipoitem x;
 apontador pos;
 
@@ -35,6 +35,28 @@ int Vazia (tipolista Lista)
     return (Lista.Primeiro == Lista.Ultimo);
 }
 
+void insere (tipoitem x, tipolista *Lista)
+{
+    if (Lista->Ultimo > Maxtam)
+    {
+        printf("Erro: Lista cheia\n");
+        return;
+    }
+    Lista->item[Lista->Ultimo -1] = x;
+    Lista->Ultimo++;
+}
+
+void imprime (tipolista Lista)
+{
+    int aux;
+    for (aux = Lista.Primeiro-1; aux < Lista.Ultimo-2; aux++)
+    {
+        printf("Codigo: %d\n", Lista.item[aux -1].cod);
+        printf("Idade: %d\n", Lista.item[aux -1].idade);
+    }
+    
+}
+
 int main (){
 
     int opcao ,resposta;
@@ -42,11 +64,13 @@ int main (){
     // FLVazia(&lista);
     do 
     {
-        printf("\n\nMenu\n\n");
+        printf("\nMenu\n");
         printf("\n0 = Sair");
         printf("\n1 = Esvaziar lista");
         printf("\n2 = Verificar se a lista esta vazia");
-        printf("Digite a opcao desejada : ");
+        printf("\n3 = Inserir elemento na lista");
+        printf("\n4 = imprimir os elemntos da lista");
+        printf("\nDigite a opcao desejada : ");
         scanf("%d",&opcao);
         switch (opcao)
         {
@@ -67,6 +91,21 @@ int main (){
                 {
                     printf("A lista nao esta vazia!\n");
                 }
+                break;
+            case 3:
+                {
+                    printf("Digite o codigo: ");
+                    scanf("%d",&x.cod);
+                    printf("Digite a idade: ");
+                    scanf("%d",&x.idade);
+                    insere(x, &Lista);
+                    break;
+                }
+            case 4:
+            {
+                imprime(Lista);
+                break;
+            }
                 break;
             default:
                 printf("Opcao invalida! Tente novamente.\n");
